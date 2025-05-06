7165fccc3672a7c1ae6a4fcdd4d1d90a7f7f26d8
oduncica <oduncica@gmail.com>
2025-05-06 11:33:00 +0900

    add build files

--- src/Intro.jsx
+++ src/Intro.jsx
@@ -18,21 +18,21 @@ function Intro() {
           <h2>Le documentaire interactif</h2>
 
           <div className="paragraphs">
-            <p className="typewriter" style={{ animationDelay: "0s" }}>
+            <p>
               In South Korea, the regulatory culture in high schools deeply
               shapes students’ daily lives.
             </p>
-            <p className="typewriter" style={{ animationDelay: "3s" }}>
+            <p>
               We invite you to step into the personal spaces of South Korean
               high schoolers across generations. A student’s room from 1980,
               from the 2000s and from 2025.
             </p>
-            <p className="typewriter" style={{ animationDelay: "6s" }}>
+            <p>
               These rooms act as time capsules, revealing how discipline,
               societal expectations, and daily routines have evolved... or
               stayed the same.
             </p>
-            <p className="typewriter" style={{ animationDelay: "9s" }}>
+            <p>
               Each space tells a story, not only about education, but also about
               identity, resistance, pressure and adaptation under a powerful
               regulatory system.
